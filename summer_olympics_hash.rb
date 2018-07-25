feb28e6e8ea69ae2ca064f5dbdb5d0051f68cbd8
kate harris <kateharris03@gmail.com>
2018-07-25 19:56:49 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -17,7 +17,7 @@ end
 puts add_a_key_value_pair
 
 def iterate_through_hash
-  new_summer_olympics.each do |place, year|
+  add_a_key_value_pair.each do |place, year|
     puts "The #{place} Olympics took place in #{year}."
   end
 end
