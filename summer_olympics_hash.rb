0283b1c51ee5125b684a708e5641933f29c93ccd
kate harris <kateharris03@gmail.com>
2018-07-25 19:48:23 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -12,12 +12,14 @@ create_olympics_hash
 def add_a_key_value_pair
   summer_olympics[:Atlanta => 1996]
 end
+add_a_key_value_pair
 
 def iterate_through_hash
   summer_olympics.each do |place, year|
     puts "The #{place} Olympics took place in #{year}."
   end
 end
+iterate_through_hash
 
 # def iterate_through_keys
 #   summer_olympics.each_key {|key| puts key}
